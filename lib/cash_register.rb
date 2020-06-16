a289397e1e0b992c351b80e4a35c629df83a4154
Samantha Barros <samantha.barros95@gmail.com>
2020-06-16 06:39:54 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   
   def add_item(title, price, quantity = 1)
      self.total += price * quantity
-     #last_transaction_amount = self.total
+     last_transaction_amount = self.total
   end 
   
   def apply_discount
@@ -32,7 +32,7 @@ class CashRegister
  end 
  
  def void_last_transaction
-    self.total #-= last_transaction_amount
+    self.total -= last_transaction_amount
   end
 
 end 
