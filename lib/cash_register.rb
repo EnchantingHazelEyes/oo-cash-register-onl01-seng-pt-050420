f8dcfcdecee1d222db5168659d1e25c7e4c468ea
Samantha Barros <samantha.barros95@gmail.com>
2020-06-16 06:09:10 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,6 +14,7 @@ class CashRegister
   end 
   
   def apply_discount
+    discount = 
     discounted_total = total - total * discount/100
     if discount == 0 
       "There is no discount to apply."  
