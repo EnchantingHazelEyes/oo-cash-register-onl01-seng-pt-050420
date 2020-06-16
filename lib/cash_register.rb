3a84c3e0c4be9aa1dd3267d1835e0c77d46663c8
Samantha Barros <samantha.barros95@gmail.com>
2020-06-16 05:41:31 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,27 @@
+class CashRegister
 
+  attr_accessor :total, :discount, :title, :new_total, :item_list
+  
+  def initialize(discount = 0)
+    @total = total || 0
+    @discount = discount
+    @title = title
+    @item_list = []
+  end 
+  
+  def add_item(title, price, quantity = 1)
+      self.total += price * quantity
+      while quantity < 1 
+      item_list << title
+      +1
+    end 
+  end 
+    
+  def items
+   new_list = []
+   item_list.each do |item|
+   new_list << item  
+  end 
+ end 
+
+end 
