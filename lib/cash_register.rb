7a4f8a736b2693f0597c415ab2e2d9fc6648fe1a
Samantha Barros <samantha.barros95@gmail.com>
2020-06-16 06:04:42 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,7 @@ class CashRegister
     if discount == 0 
       "There is no discount to apply."  
     else
-    "After the discount, the total comes to #{discounted_total}."
+    "After the discount, the total comes to $#{discounted_total}."
     
     end 
       
