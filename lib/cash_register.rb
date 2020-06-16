5452a71507b75229ac8ead8bd4d18195248fe187
Samantha Barros <samantha.barros95@gmail.com>
2020-06-16 06:32:33 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,6 +19,7 @@ class CashRegister
       "There is no discount to apply."  
     else
     "After the discount, the total comes to $#{discounted_total}."
+        discounted_total = @total - (@total * @discount)/100
     end 
   end 
     
